d86f18fcbb88c658c15ef621da7484ed428195ac
Jay <rright815@gmail.com>
2023-05-17 00:40:58 +1000

    navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Announcement from './components/Announcement'
+import Navbar from './components/Navbar'
+
+function Home() {
+  return (
+    <div>
+        <Announcement></Announcement>
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Home
