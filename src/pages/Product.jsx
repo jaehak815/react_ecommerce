507d41821bb1320ae3ff175469db3a6b50b0d1e0
Jay <rright815@gmail.com>
2023-05-27 22:45:16 +1000

    redux_shoppingCart

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,15 +1,16 @@
 import { Add, Remove } from "@material-ui/icons";
-import axios from "axios";
 import { useEffect } from "react";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { styled } from "styled-components";
+import { addProduct } from "../redux/cartRedux";
 import { publicRequest } from "../requestMethods";
 import { mobile } from "../responsive";
 import Announcement from "./components/Announcement";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Newsletter from "./components/Newsletter";
+import {useDispatch} from "react-redux";
 
 const Container = styled.div``;
 const Wrapper = styled.div`
@@ -112,6 +113,7 @@ const Product = () => {
   const [quantity,setQuantity] = useState(1);
   const [color,setColor] = useState("");
   const [size,setSize] = useState("");
+  const dispatch = useDispatch()
 
   useEffect(()=>{
     const getProduct = async ()=>{
@@ -129,6 +131,14 @@ const Product = () => {
     } else {
         setQuantity(quantity+1);
     }
+  };
+  
+  const handleClick =()=>{
+    dispatch(
+
+        addProduct({...product, quantity, color, size})
+    );
+
   };
  
   return (
@@ -170,7 +180,7 @@ const Product = () => {
                         <Amount>{quantity}</Amount>
                         <Add onClick={()=>handleQuantity("inc")}/>
                     </AmountContainer>
-                    <Button>ADD TO CART</Button>
+                    <Button onClick={handleClick}>ADD TO CART</Button>
                 </AddContainer>
             </InfoContainer>
         </Wrapper>
